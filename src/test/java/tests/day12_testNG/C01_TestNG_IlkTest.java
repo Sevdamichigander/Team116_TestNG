f7e6fc632f5e3e898eb001643699b289a3d7ad57
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-07-06 12:00:00 +0200

    driver_reusableMethods

---
+++ src/test/java/tests/day12_testNG/C01_TestNG_IlkTest.java
@@ -0,0 +1,35 @@
+package tests.day12_testNG;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C01_TestNG_IlkTest {
+    @Test
+    public void test01(){
+
+        /*
+        Page Object Model(POM) de driver olusturmak ve gerekli ayarlari yapmak icin
+         @Testbase kullanmak yerine,
+        ayri bir driver class i icerisinde
+        bize driver dondurecek bir method olusturulmasi tercih edilmistir.
+         */
+
+        Driver.getDriver().get("https://www.amazon.com");
+
+        ReusableMethods.bekle(5);
+
+        Driver.closeDriver();
+
+        /*
+        ===============================================
+        Default Suite
+        Total tests run: 1, Passes: 1, Failures: 0, Skips: 0
+        ===============================================
+         */
+
+
+
+
+    }
+}
